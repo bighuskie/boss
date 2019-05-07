f91b5503cfba4a8340ff5114bf7c1371b9fae238
B.Huskie <1665370368@qq.com>
2019-05-07 08:37:15 +0800

    使用已有的后台作为项目的后端服务

--- server/app.js
+++ server/app.js
@@ -1,23 +1,41 @@
-const express = require("express");
-const app = express();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const userRouter = require("./routers/user");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-//设置允许跨域访问该服务.
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "*");
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
-});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-//中间件的执行顺序有要求
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use("/user", userRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(8080, function() {
-  console.log("server running");
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
